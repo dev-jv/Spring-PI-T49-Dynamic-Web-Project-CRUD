2eb9fefd7a00beb53f575f1fb5a70483fd838246
dev-jv <dev.javierleon@gmail.com>
2020-11-21 22:34:53 -0500

    one

---
+++ src/connection/ConnectCRUD.java
@@ -0,0 +1,72 @@
+package connection;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ConnectCRUD
+ */
+@WebServlet("/ConnectCRUD")
+public class ConnectCRUD extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ConnectCRUD() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		String jdbcurl="jdbc:mysql://localhost:3306/ordermanagementcrud?useSSL=false";
+		
+		String user = "root";
+		
+		String password = "";
+		
+		String driver="com.mysql.cj.jdbc.Driver";
+//		String driver="com.mysql.jdbc.Driver";
+		
+		try {
+			PrintWriter out = response.getWriter();
+			
+			out.print("DB: " + jdbcurl + "<br>");
+			
+			Class.forName(driver);
+			
+			Connection myConnection = DriverManager.getConnection(jdbcurl, user, password);
+			
+			out.print("CONECTED!!");
+			
+			myConnection.close();
+			
+		} catch(Exception e) {
+			
+			e.printStackTrace();
+		} 
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
